f2456d49581f01aaf6a121317166f6814a1828a3
SonsOfTone <dubost.bulard.antoine@gmail.com>
2014-11-01 09:52:29 +0100

    Update start script

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/opt/vc/lib:$LD_LIBRARY_PATH
-/home/pi/RaspberrIPCam -o - -t 0 -b 5000000 -w 1280 -h 720 -ss 30000 -g 15 -fps 30
+/home/pi/RaspberrIP-Camera/RaspberrIPCam -o - -t 0 -b 5000000 -w 1280 -h 720 -ss 30000 -g 15 -fps 30
